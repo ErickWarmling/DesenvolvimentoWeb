3ce9fa0ead6c1055da95b53f7368c3ee56d80c8d
ErickWarmling <warmling.erick@gmail.com>
2025-04-24 17:38:43 -0300

    Exercício API REST

---
+++ exercicio-api-rest/src/main/java/com/erick/warmling/exercicio_api_rest/repository/ProdutoRepository.java
@@ -0,0 +1,9 @@
+package com.erick.warmling.exercicio_api_rest.repository;
+
+import com.erick.warmling.exercicio_api_rest.model.Produto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+}
