30340b5f06c3a90e9964206fbd9a0210ac1339b2
Erick Augusto Warmling <135060514+ErickWarmling@users.noreply.github.com>
2025-04-26 16:04:28 -0300

    Delete exercicio-api-rest directory

--- exercicio-api-rest/src/main/java/com/erick/warmling/exercicio_api_rest/repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.erick.warmling.exercicio_api_rest.repository;
-
-import com.erick.warmling.exercicio_api_rest.model.Cliente;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-}
--- exercicio-api-rest/src/main/java/com/erick/warmling/exercicio_api_rest/repository/FornecedorRepository.java
+++
@@ -1,9 +0,0 @@
-package com.erick.warmling.exercicio_api_rest.repository;
-
-import com.erick.warmling.exercicio_api_rest.model.Fornecedor;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
-}
