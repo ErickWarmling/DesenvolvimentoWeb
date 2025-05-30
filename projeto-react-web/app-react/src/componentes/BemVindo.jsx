744c7f3038d4557cd230259ef95ea344806bcc81
ERICK AUGUSTO WARMLING <09644564944@edu.udesc.br>
2025-05-30 20:37:12 -0300

    Projeto React Web

--- projeto-react-web/app-react/src/componentes/BemVindo.jsx
+++ projeto-react-web/app-react/src/componentes/BemVindo.jsx
@@ -1,12 +1,32 @@
 import { Link, useParams } from "react-router-dom"
+import axios from "axios";
 
 export default function BemVindo() {
     const params = useParams();
+
+    function iniciar() {
+        axios.get('http://localhost:8080/inicio')
+            .then((resposta) => sucesso(resposta))
+            .catch((erro) => falhou(erro))
+            .finally( () => console.log('Finalizou a chamada...'));
+    }
+
+    function sucesso(resposta) {
+        console.log('Resposta: ');
+        console.log(resposta); 
+    }
+    
+    function falhou(erro) {
+        console.log('Erro: ');
+        console.log(erro);
+    }
+    
 
     return (
     <>
         <h1>Olá, {params.usuario}</h1>
         <Link to='/tarefas'>Tarefas</Link>
+        <button onClick={iniciar}>Bem-Vindo</button>
     </>
     )
 }
