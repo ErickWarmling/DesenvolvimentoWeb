03db26622c3853d5cfe814d3777c87e1d6291d30
ERICK AUGUSTO WARMLING <09644564944@edu.udesc.br>
2025-07-04 20:31:01 -0300

    Delete Projeto Rest

--- projeto-rest/src/main/java/dsw/projeto_rest/jpa/TarefaRepository.java
+++
@@ -1,9 +0,0 @@
-package dsw.projeto_rest.jpa;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import dsw.projeto_rest.user.Tarefa;
-
-public interface TarefaRepository extends JpaRepository<Tarefa, Integer>{
-
-}
